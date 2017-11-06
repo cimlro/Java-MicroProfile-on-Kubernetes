aef0a99c42ed6b33f374cb63d360ff10a2244c49
Luc ROTSAERT <BE02924@Lucs-MacBook-Pro-2.local>
2017-11-06 18:15:35 +0100

    change WASDEV with cimlro

--- scripts/get_code.sh
+++ scripts/get_code.sh
@@ -5,17 +5,17 @@ function mvn_clean() {
   cd ..
 }
 
-git clone https://github.com/WASdev/sample.microservicebuilder.web-app
+git clone https://github.com/cimlro/sample.microservicebuilder.web-app
 mvn_clean sample.microservicebuilder.web-app
 
-git clone https://github.com/WASdev/sample.microservicebuilder.schedule
+git clone https://github.com/cimlro/sample.microservicebuilder.schedule
 mvn_clean sample.microservicebuilder.schedule
 
-git clone https://github.com/WASdev/sample.microservicebuilder.speaker
+git clone https://github.com/cimlro/sample.microservicebuilder.speaker
 mvn_clean sample.microservicebuilder.speaker
 
-git clone https://github.com/WASdev/sample.microservicebuilder.session
+git clone https://github.com/cimlro/sample.microservicebuilder.session
 mvn_clean sample.microservicebuilder.session
 
-git clone https://github.com/WASdev/sample.microservicebuilder.vote
+git clone https://github.com/cimlro/sample.microservicebuilder.vote
 mvn_clean sample.microservicebuilder.vote
